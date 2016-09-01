9dc270f80f7451cea37c159d7867efd4adcc374d
LinMingjie <code@linmj.com>
2016-09-01 15:56:15 +0800

    Homework 1

---
+++ hw/hw01/quiz/quiz01.py
@@ -0,0 +1,27 @@
+def multiple(a, b):
+    """Return the smallest number n that is a multiple of both a and b.
+
+    >>> multiple(3, 4)
+    12
+    >>> multiple(14, 21)
+    42
+    """
+    "*** YOUR CODE HERE ***"
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(1313131) # 1 and 3
+    2
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(10000) # 0 and 1
+    2
+    >>> unique_digits(101) # 0 and 1
+    2
+    >>> unique_digits(10) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
