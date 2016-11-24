970c9599033ea8888b225ec2ccb67f3ef92a00d5
LinMingjie <code@linmj.com>
2016-11-24 23:29:19 +0800

    Lab 12 finished

--- lab/lab12/lab12.sql
+++ lab/lab12/lab12.sql
@@ -2,16 +2,19 @@
 .read fa16data.sql
 
 CREATE TABLE obedience AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select seven, denero from students;
 
 CREATE TABLE smallest_int AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select time, smallest from students where smallest > 8 order by smallest limit 20;
 
 CREATE TABLE greatstudents AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select fa.date, fa.number, fa.pet, fa.color, sp.color from students as fa, sp16students as sp 
+  where fa.date = sp.date and fa.number = sp.number and fa.pet = sp.pet;
 
 CREATE TABLE sevens AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select s.seven from students as s, checkboxes as b 
+  where s.time = b.time and s.number = 7 and b.'7' = 'True';
 
 CREATE TABLE matchmaker AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select a.pet, a.song, a.color, b.color from students as a, students as b 
+  where a.time < b.time and a.pet = b.pet and a.song = b.song;
--- lab/lab12/lab12_extra.sql
+++ lab/lab12/lab12_extra.sql
@@ -1,27 +1,27 @@
 .read lab12.sql
 
 CREATE TABLE sp16favnum AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  select number, count(*) as count from sp16students 
+  group by number order by count desc limit 1;
 
 CREATE TABLE sp16favpets AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  select pet, count(*) as count from sp16students
+  group by pet order by count desc limit 10;
 
 CREATE TABLE fa16favpets AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  select pet, count(*) as count from students
+  group by pet order by count desc limit 10;
 
 CREATE TABLE fa16dragon AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  select pet, count(*) from students where pet = 'dragon';
 
 CREATE TABLE fa16alldragons AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  select pet, count(*) from students where pet like '%dragon%';
 
 CREATE TABLE obedienceimage AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select seven, denero, count(*) from students where seven = '7' 
+  group by denero;
 
 CREATE TABLE smallest_int_count AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select smallest, count(*) from students where smallest >= 1 
+  group by smallest;
